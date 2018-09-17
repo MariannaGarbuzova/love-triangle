014f298e0e38756f0fea0c98e49a96892cb33c2c
MariannaGarbuzova <vesna09@tut.by>
2018-09-17 20:20:21 +0300

    trangle implementaton

--- src/index.js
+++ src/index.js
@@ -3,5 +3,39 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  preferences = [0, ...preferences];
+  var count = 0;
+  var i;
+  for (i = 1; i < preferences.length; i++) {
+    var isTriangle = checkTrianglesByIndex(i, preferences);
+    if (isTriangle) {
+      count++;
+    }
+  }
+
+  return count / 3;
 };
+
+function checkTrianglesByIndex(index, preferences) {
+  //let index = 1;
+  let R = preferences;
+  let lover1index = index;
+  let lover1loves = R[lover1index];
+
+  if (lover1index === lover1loves) return false;
+
+  let lover2index = lover1loves;
+  let lover2loves = R[lover2index];
+
+  if (lover2index === lover2loves) return false;
+
+  let lover3index = lover2loves;
+  let lover3loves = R[lover3index];
+
+  if (lover3loves === lover3index) return false;
+
+  if (lover3loves === lover1index)
+    return true;
+  else
+    return false;
+}
